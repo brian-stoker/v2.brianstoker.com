d2c1b703ab1cfc8e5b743edfed043597475cdccd
Brian Stoker <b@stokedconsulting.com>
2025-04-08 11:09:16 -0500

    merge success with main.. 1 left to go

--- src/components/home/ImageShowcase.tsx
+++ src/components/home/ImageShowcase.tsx
@@ -3,5 +3,5 @@ import MediaShowcase from './MediaShowcase';
 
 
 export default function ImageShowcase({ showcaseContent }: { showcaseContent?: any }): React.JSX.Element {
-  return <MediaShowcase sx={{ height: '520px'}} showcaseContent={<img src={showcaseContent} alt={''} style={{borderRadius: '12px', overflow: 'hidden', objectFit: 'cover' }}/>} />
+  return <MediaShowcase sx={{ width: 'fit-content','& img': { height: '720px'}}} showcaseContent={<img src={showcaseContent} alt={''} style={{borderRadius: '12px', overflow: 'hidden', objectFit: 'cover' }}/>} />
 }
