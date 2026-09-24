2152dedb2f72f32aa3f6b40fff873ba33b7ed360
Brian Stoker <b@stokedconsulting.com>
2026-09-24 16:25:01 -0500

    Add 2026-09-22 .plan and drop internal hostname from Hal post

--- data/pages.ts
+++ data/pages.ts
@@ -69,6 +69,10 @@ const pages: MuiPage[] = [
     pathname: '/.plan/2026-09-21-selfagent-desktop-session-cards-lander.mdx',
     title: 'selfagent desktop, session cards, lander',
   },
+  {
+    pathname: '/.plan/2026-09-22-gdock-left-rail-lander-still-the-job.mdx',
+    title: 'gdock left rail, lander still the job',
+  },
 ];
 
 export default pages;
